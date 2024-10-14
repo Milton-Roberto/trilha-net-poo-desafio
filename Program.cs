809f0d7231102d83f8e4d82e41544ffa73c8aa85
Milton Roberto <miltinroberto192@gmail.com>
2024-10-14 12:41:01 -0300

    Desafio concluidooooo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokiaTijoalao = new Nokia("123456789", "Tijolão", "11111", 12 );
+Iphone iphoneNovo = new Iphone("987654321", "Iphone15", "22222", 64 );
+
+nokiaTijoalao.Ligar();
+nokiaTijoalao.ReceberLigacao();
+nokiaTijoalao.InstalarAplicativo("WhatsApp");
+
+iphoneNovo.Ligar();
+iphoneNovo.ReceberLigacao();
+iphoneNovo.InstalarAplicativo("Instagram");
